b828511cd6f4b3f958fae28152008db37a96fd4d
Marco <mail@iconxtreme.net>
2016-09-23 09:29:10 +0200

    Update

--- public/index.php
+++ public/index.php
@@ -4,3 +4,14 @@
  */
 
 require_once '../transcendence.php';
+
+
+$lessfiles = array( 
+		dirname(__FILE__).'/style/less/style.less' => '/style/less/' 
+	);
+
+$less_vars = array();
+
+
+
+echo start($lessfiles, $less_vars);
--- transcendence.php
+++ transcendence.php
@@ -13,52 +13,60 @@ require_once 'config/general.php';
 require_once __DIR__.'/vendor/autoload.php';
 
 
-// LESS Compiler
-try{
-	$less_files 	= array( dirname(__FILE__).'/public/style/less/style.less' => '/style/less/' );
-	$options 		= array( 'cache_dir' => dirname(__FILE__).'/public/style/cache' );
-	$variables 		= array(); //eg. array( 'width' => '100px' );
-	$css_file_name 	= '/style/cache/'. Less_Cache::Get( $less_files, $options, $variables );
-}catch(Exception $e){
-	$error_message 	= $e->getMessage();
-}
 
+function start( $less_files = array(), $less_vars = array() ) {
 
-// Twig Compiler
-try{
-	Twig_Autoloader::register();
-	
-	$loader = new Twig_Loader_Filesystem('../pages');
-	$loader->addPath('../templates', 'templates');
-	$loader->addPath('../partials', 'partials');
-	
-	if($TWIG_CACHE){
-		$twig = new Twig_Environment($loader, array('cache' => '../cache',));
-	}else{
-		$twig = new Twig_Environment($loader);
+	// LESS Compiler
+	try{
+		$options 		= array( 'cache_dir' => dirname(__FILE__).'/public/style/cache' );
+		$css_file_name 	= '/style/cache/'. Less_Cache::Get( $less_files, $options, $less_vars );
+	}catch(Exception $e){
+		$error_message 	= $e->getMessage();
 	}
-}catch(Exception $e){
-	$error_message 	= $e->getMessage();
-}
+	
+	
+	// Twig Compiler
+	try{
+		Twig_Autoloader::register();
+		
+		$loader = new Twig_Loader_Filesystem('../pages');
+		$loader->addPath('../templates', 'templates');
+		$loader->addPath('../partials', 'partials');
+		
+		if($TWIG_CACHE){
+			$twig = new Twig_Environment($loader, array('cache' => '../cache',));
+		}else{
+			$twig = new Twig_Environment($loader);
+		}
+	}catch(Exception $e){
+		$error_message 	= $e->getMessage();
+	}
+	
+	
+	$twig_vars 	= array('cssfile' => $css_file_name, 'sessionid' => $sessionid );
+	
+	
+	$url 		= $_SERVER["REQUEST_URI"];
+	$url		= substr($url, 1);
+	
+	$page_url	= explode("/",$url);
+	$page_end 	= end($page_url);
+	
+	if($page_end == ''){
+		$url = $url."index";
+	}
+	
+	$html = dirname(__FILE__).'/pages/'.$url.'.html';
+	
+	try{
+		if(file_exists($html)){
+			echo $twig->render($url.'.html', $twig_vars );
+		}else{
+			echo $twig->render('404.html', $twig_vars );
+		}
+	}catch(Exception $e){
+		$error_message 	= $e->getMessage();
+	}
 
-
-$twig_vars 	= array('cssfile' => $css_file_name, 'sessionid' => $sessionid );
-
-
-$url 		= $_SERVER["REQUEST_URI"];
-$url		= substr($url, 1);
-
-$page_url	= explode("/",$url);
-$page_end 	= end($page_url);
-
-if($page_end == ''){
-	$url = $url."index";
-}
-
-$html = dirname(__FILE__).'/pages/'.$url.'.html';
-
-if(file_exists($html)){
-	echo $twig->render($url.'.html', $twig_vars );
-}else{
-	echo $twig->render('404.html', $twig_vars );
+	return $error_message;
 }
