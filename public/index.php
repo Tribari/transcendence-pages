519b7b8ab3e5bdfd2ca5ead9628005cb226a9c6a
Marco <mail@iconxtreme.net>
2016-09-27 16:27:02 +0200

    Update

--- public/index.php
+++ public/index.php
@@ -10,9 +10,13 @@ $lessfiles = array(
 		dirname(__FILE__).'/style/less/style.less' => '/style/less/' 
 	);
 
+$scriptfiles = array(
+		dirname(__FILE__).'/style/js/script.js'
+);
+
 $less_vars = array();
 
-$twig_vars = array();
+$twig_vars = array( 'scripts' => $scriptfiles );
 
 $twig_cache = false;
 
